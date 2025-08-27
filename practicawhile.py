92472270a6fda5c5ff5400cd814bef38d4e45141
ally1507 <226592771+ally1507@users.noreply.github.com.>
2025-08-27 23:53:04 +0000

    aprendi git

---
+++ practicawhile.py
@@ -0,0 +1,11 @@
+for i in range (2, 14, 2):
+    print(f"El valor del bucle es {i}")
+
+animales= [ "perro", "gato", "oso"]
+print("listado de animales")
+
+for animal in animales:
+    if animal == "perro" or animal == "gato" or animal == "oso" :
+        
+
+
